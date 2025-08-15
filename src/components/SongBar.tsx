d3ba5cb987c8ab44d024f917e47dbb461f5775de
Leonardotorre18 <Leonardotorre14@gmail.com>
2025-08-14 21:45:07 -0400

    feat: processbar

--- src/components/SongBar.tsx
+++ src/components/SongBar.tsx
@@ -1,48 +1,111 @@
-// import type { ISong } from "../types/song"
-import React from 'react'
+// import type { ISong } from '../types/song'
+import React, { type ChangeEvent, type SyntheticEvent } from 'react'
 import './SongBar.css'
 import { CurrentSong } from '../store/store'
 import type { ISong } from '../types/song'
+import { IoPause, IoPlay } from 'react-icons/io5'
 
 export default function () {
-    const [song, setSong] = React.useState<ISong | null>(null)
+  const [song, setSong] = React.useState<ISong | null>(null)
+  const [play, setPlay] = React.useState<boolean>(false)
+  const [progress, setProgress] = React.useState<number>(0)
 
-    React.useEffect(() => {
-        CurrentSong.subscribe(song => {
-            setSong(song)
-        })
-    }, [])
+  const audioRef = React.useRef<HTMLAudioElement | null>(null)
 
-    const handlerClick = () => {
-        console.log('Funcionando')
+  React.useEffect(() => {
+    CurrentSong.subscribe(song => {
+      setSong(song)
+      setPlay(true)
+    })
+  }, [])
+
+  React.useEffect(() => {
+    const intervalID = setInterval(() => updateProgress(), 1000)
+    return () => {
+      clearInterval(intervalID)
     }
-    return (
-        <>
-            <div className="song-bar" onClick={handlerClick}>
-                {
-                    song
-                        ? <div className="song-bar__info">
-                            <div className="song-bar__info__image">
-                                <img src={song.image.url} alt="" />
-                            </div>
-                            <div className="song-bar__info__details">
-                                <h4 className="song-bar__info__details__title">{song.title}</h4>
-                                <p className="song-bar__info__details__artist">{song.author}</p>
-                            </div>
-                        </div>
-                        : <></>
-                }
-                <div className="song-bar__controls">
-                    <div className="song-bar__controls__buttons">
-                        {
-                            song
-                                ? <audio src={song.audio.url} id="audio" controls autoPlay />
-                                : <></>
-                        }
-                    </div>
-                </div>
-                <div className="song-bar__extra"></div>
+  }, [song])
+
+  const updateProgress = () => {
+    if (audioRef.current) {
+      const { duration, currentTime } = audioRef.current
+      if (duration > 0) {
+        const percent = (currentTime * 100) / duration
+        setProgress(percent)
+      } else {
+        setProgress(0)
+      }
+    }
+  }
+
+  const handlerProcessbar = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget
+    if (audioRef.current && audioRef.current.duration) {
+      const newTime = (parseFloat(value) / 100) * audioRef.current.duration
+      audioRef.current.currentTime = newTime
+      setProgress(parseFloat(value))
+    }
+  }
+
+  const handlerClickPlay = () => {
+    if (audioRef.current) {
+      setProgress(audioRef.current.currentTime)
+      if (audioRef.current.paused) {
+        audioRef.current.play()
+        setPlay(true)
+        updateProgress()
+      } else {
+        audioRef.current.pause()
+        setPlay(false)
+        updateProgress()
+      }
+    }
+  }
+
+  return (
+    song
+      ? <>
+        <div className="song-bar">
+          <div className="song-bar__info">
+            <div className="song-bar__info__image">
+              <img src={song.image.url} alt="" />
             </div>
-        </>
-    )
+            <div className="song-bar__info__details">
+              <h4 className="song-bar__info__details__title">{song.title}</h4>
+              <p className="song-bar__info__details__artist">{song.author}</p>
+            </div>
+          </div>
+          <audio
+            src={song.audio.url}
+            autoPlay
+            ref={audioRef}
+            onTimeUpdate={updateProgress}
+            onLoadedMetadata={updateProgress}
+          />
+          <div className="song-bar__controls">
+            <div className="song-bar__controls__process">
+              <input
+                type="range"
+                min={0}
+                max={100}
+                value={progress}
+                onChange={handlerProcessbar}
+              />
+
+            </div>
+            <div className='song-bar__controls__buttons'>
+              <button>
+                {
+                  play
+                    ? <IoPause onClick={handlerClickPlay} />
+                    : <IoPlay onClick={handlerClickPlay}/>
+                }
+              </button>
+
+            </div>
+          </div>
+        </div>
+      </>
+      : <></>
+  )
 }
